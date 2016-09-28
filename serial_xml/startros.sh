2b30559d7ba4d2078ee8f19ee1dc5ee198f42371
beamiter <beamiter@163.com>
2016-09-28 13:34:20 +0800

    bug solving

--- serial_xml/startros.sh
+++ serial_xml/startros.sh
@@ -2,5 +2,9 @@
 gnome-terminal -x $PWD/serial_xml/serialport.bash
 source /opt/ros/jade/setup.bash
 source /usr/local/serial/setup.bash
-source /home/$USR/ivpt/devel/setup.bash
-roslaunch /home/$USR/ivpt/src/launch/start.launch
+
+ivptdir=/home/$USER/ivpt/devel/setup.bash
+launchdir=/home/$USER/ivpt/src/launch/start.launch
+
+source ${ivptdir}
+roslaunch ${launchdir}
